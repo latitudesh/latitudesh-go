69d2085112a6bc773ea519e568273b56457a2039
ynhummel <yurihummel@outlook.com>
2023-02-24 16:10:22 -0300

    chore: update tests

--- operating_systems_test.go
+++ operating_systems_test.go
@@ -19,13 +19,13 @@ func TestAccOperatingSystemBasic(t *testing.T) {
 	}
 
 	OsTest := OperatingSystem{
-		ID:       "6",
+		ID:       "7",
 		Type:     "operating_system",
-		Name:     "CentOS 8",
+		Name:     "CentOS 7.4",
 		Distro:   "centos",
-		Slug:     "centos_8_x64",
-		Version:  "8",
-		User:     "cloud-user",
+		Slug:     "centos_7_4_x64",
+		Version:  "7.4",
+		User:     "centos",
 		Raid:     true,
 		Rescue:   true,
 		SshKeys:  true,
@@ -41,5 +41,29 @@ func TestAccOperatingSystemBasic(t *testing.T) {
 	}
 	if OsTest.Name != osl[0].Name {
 		t.Fatalf("Expected the name of the Operating System to be %s, not %s", OsTest.Name, osl[0].Name)
+	}
+	if OsTest.Distro != osl[0].Distro {
+		t.Fatalf("Expected the name of the Operating System to be %s, not %s", OsTest.Distro, osl[0].Distro)
+	}
+	if OsTest.Slug != osl[0].Slug {
+		t.Fatalf("Expected the name of the Operating System to be %s, not %s", OsTest.Slug, osl[0].Slug)
+	}
+	if OsTest.Version != osl[0].Version {
+		t.Fatalf("Expected the name of the Operating System to be %s, not %s", OsTest.Version, osl[0].Version)
+	}
+	if OsTest.User != osl[0].User {
+		t.Fatalf("Expected the name of the Operating System to be %s, not %s", OsTest.User, osl[0].User)
+	}
+	if OsTest.Raid != osl[0].Raid {
+		t.Fatalf("Expected the name of the Operating System to be %t, not %t", OsTest.Raid, osl[0].Raid)
+	}
+	if OsTest.Rescue != osl[0].Rescue {
+		t.Fatalf("Expected the name of the Operating System to be %t, not %t", OsTest.Rescue, osl[0].Rescue)
+	}
+	if OsTest.SshKeys != osl[0].SshKeys {
+		t.Fatalf("Expected the name of the Operating System to be %t, not %t", OsTest.SshKeys, osl[0].SshKeys)
+	}
+	if OsTest.UserData != osl[0].UserData {
+		t.Fatalf("Expected the name of the Operating System to be %t, not %t", OsTest.UserData, osl[0].UserData)
 	}
 }
