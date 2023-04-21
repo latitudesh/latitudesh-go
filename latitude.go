dfccc93a1f0e4ab491ad48436ef68cd529c86b3b
mujtaba-saboor <mujtabasaboor27@gmail.com>
2023-04-21 15:09:02 +0500

    user-agent-updated

--- latitude.go
+++ latitude.go
@@ -24,6 +24,8 @@ const (
 	userAgentForProvider = "Latitude-Terraform-Provider"
 )
 
+var currentVersion = "0.2.0"
+
 // meta contains pagination information
 type meta struct {
 	Self           *Href `json:"self"`
@@ -128,8 +130,8 @@ func (c *Client) NewRequest(method, path string, body interface{}) (*http.Reques
 
 	// set User-Agent value for SDK or terraform-provider
 	userAgent := c.UserAgent
-	if userAgent != userAgentForProvider {
-		userAgent = userAgentForSDK
+	if !strings.Contains(userAgent, userAgentForProvider) {
+		userAgent = fmt.Sprintf("%s/%s", userAgentForSDK, currentVersion)
 	}
 	req.Header.Add("User-Agent", userAgent)
 
