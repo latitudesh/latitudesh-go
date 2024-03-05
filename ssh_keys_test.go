8cf983041b1858d5b6b46f9abc46e4eb370024b3
Yuri Hummel <yurihummel@outlook.com>
2024-03-05 10:50:05 -0300

    chore: refactor user data and ssh key tests

--- ssh_keys_test.go
+++ ssh_keys_test.go
@@ -21,79 +21,88 @@ func TestAccSSHKeyBasic(t *testing.T) {
 	c, projectID, teardown := setupWithProject(t)
 	defer teardown()
 
-	// Create a new SSH Key
-	keyName := randString8()
-	skcr := SSHKeyCreateRequest{
-		Data: SSHKeyCreateData{
-			Type: testSSHKeyType,
-			Attributes: SSHKeyCreateAttributes{
-				Name:      keyName,
-				PublicKey: testSSHKey(),
+	var keyID string
+
+	t.Run("Create SSH Key", func(t *testing.T) {
+		keyName := randString8()
+		skcr := SSHKeyCreateRequest{
+			Data: SSHKeyCreateData{
+				Type: testSSHKeyType,
+				Attributes: SSHKeyCreateAttributes{
+					Name:      keyName,
+					PublicKey: testSSHKey(),
+				},
 			},
-		},
-	}
+		}
 
-	k, _, err := c.SSHKeys.Create(projectID, &skcr)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer deleteSSHKey(t, c, k.ID, projectID)
+		k, _, err := c.SSHKeys.Create(projectID, &skcr)
+		if err != nil {
+			t.Fatal(err)
+		}
 
-	if k.Name != keyName {
-		t.Fatalf("Expected new SSH key name to be %s, not %s", keyName, k.Name)
-	}
+		if k.Name != keyName {
+			t.Fatalf("Expected new SSH key name to be %s, not %s", keyName, k.Name)
+		}
 
-	kList, _, err := c.SSHKeys.List(projectID, nil)
-	if err != nil {
-		t.Fatal(err)
-	}
+		keyID = k.ID
+	})
 
-	if len(kList) == 0 {
-		t.Fatalf("Plan List should contain at least one plan")
-	}
+	defer deleteSSHKey(t, c, keyID, projectID)
 
-	// Get first listed SSHkey
-	gotKey, _, err := c.SSHKeys.Get(kList[0].ID, projectID, nil)
-	if err != nil {
-		t.Fatal(err)
-	}
+	t.Run("Get and List SSHKeys", func(t *testing.T) {
+		kList, _, err := c.SSHKeys.List(projectID, nil)
+		if err != nil {
+			t.Fatal(err)
+		}
 
-	// Check SSHKey data
-	if gotKey.ID != kList[0].ID {
-		t.Fatalf("Expected the id of the GOT plan to be %s, not %s", kList[0].ID, gotKey.ID)
-	}
-	if gotKey.Name != kList[0].Name {
-		t.Fatalf("Expected the Name of the GOT plan to be %s, not %s", kList[0].Name, gotKey.Name)
-	}
-	if gotKey.PublicKey != kList[0].PublicKey {
-		t.Fatalf("Expected the name of the GOT plan to be %s, not %s", kList[0].PublicKey, gotKey.PublicKey)
-	}
+		if len(kList) == 0 {
+			t.Fatalf("Plan List should contain at least one plan")
+		}
 
-	// Update newly created SSH key
-	keyName = randString8()
-	skur := SSHKeyUpdateRequest{
-		Data: SSHKeyUpdateData{
-			ID:   k.ID,
-			Type: testSSHKeyType,
-			Attributes: SSHKeyUpdateAttributes{
-				Name: keyName,
+		// Get first listed SSHkey
+		gotKey, _, err := c.SSHKeys.Get(kList[0].ID, projectID, nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		// Check SSHKey data
+		if gotKey.ID != kList[0].ID {
+			t.Fatalf("Expected the id of the GOT plan to be %s, not %s", kList[0].ID, gotKey.ID)
+		}
+		if gotKey.Name != kList[0].Name {
+			t.Fatalf("Expected the Name of the GOT plan to be %s, not %s", kList[0].Name, gotKey.Name)
+		}
+		if gotKey.PublicKey != kList[0].PublicKey {
+			t.Fatalf("Expected the name of the GOT plan to be %s, not %s", kList[0].PublicKey, gotKey.PublicKey)
+		}
+	})
+
+	t.Run("Update SSH Key", func(t *testing.T) {
+		keyName := randString8()
+		skur := SSHKeyUpdateRequest{
+			Data: SSHKeyUpdateData{
+				ID:   keyID,
+				Type: testSSHKeyType,
+				Attributes: SSHKeyUpdateAttributes{
+					Name: keyName,
+				},
 			},
-		},
-	}
-	k, _, err = c.SSHKeys.Update(k.ID, projectID, &skur)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if k.Name != keyName {
-		t.Fatalf("Expected the name of the updated SSH key to be %s, not %s", keyName, k.Name)
-	}
+		}
+		k, _, err := c.SSHKeys.Update(keyID, projectID, &skur)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if k.Name != keyName {
+			t.Fatalf("Expected the name of the updated SSH key to be %s, not %s", keyName, k.Name)
+		}
 
-	kl, _, err := c.SSHKeys.List(projectID, nil)
-	if err != nil {
-		t.Fatal(err)
-	}
+		kl, _, err := c.SSHKeys.List(projectID, nil)
+		if err != nil {
+			t.Fatal(err)
+		}
 
-	if len(kl) != 1 {
-		t.Fatalf("SSH key List should contain exactly one key, was: %v", kl)
-	}
+		if len(kl) != 1 {
+			t.Fatalf("SSH key List should contain exactly one key, was: %v", kl)
+		}
+	})
 }
